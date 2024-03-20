068b339b7a35f06c315a38ec8329825c064d915e
hanacat299 <flower.catlnver63@gmail.com>
2024-03-20 09:17:49 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,24 @@
 class MessagesController < ApplicationController
   def index
+    @message = Massage.new
+    @room = Room.find(params[:room_id])
   end
+
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = aroom.messages.new(message_params)
+    if @message.save
+    redirecto_to room_messages_path(@room)
+    else
+      render :index, status: unprocessable_entity
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+  end
+
 end
